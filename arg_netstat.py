c1f92155b69f8ee1227f3f5666839a6ec6cf3c57
TLRKiliann <philogenie@protonmail.com>
2020-07-24 18:46:10 +0200

    to understand with netstat cmd

---
+++ arg_netstat.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- encoding:utf-8 -*-
+
+import argparse
+from subprocess import call
+
+"""
+Use :
+python3 arg_func.py -e tcp
+python3 arg_func.py -ep
+python3 arg_func.py -u udp
+python3 arg_func.py -uudport
+python3 arg_func.py -ep -uudport
+
+"""
+
+parser = argparse.ArgumentParser("Description : To launch netstat with options")
+parser.add_argument("-e", "--established", help="Display tcp connection established")
+parser.add_argument("-p", "--port", help="Display tcp connection established and port")
+parser.add_argument("-u", "--udpconn", help="Display udp connection established")
+parser.add_argument("--udport", help="Display udp connection established and port")
+parser.add_argument("-t", "--tab", help="Show route table")
+
+args = parser.parse_args()
+
+tcp = args.established
+udp = args.udpconn
+udp_port = args.udport
+port = args.port
+rte = args.tab
+
+if args.established:
+    call(["netstat", "-ante"])
+
+if args.udpconn:
+    call(["netstat", "-aune"]) 
+
+if args.udport:
+    call(["netstat", "-paune"]) 
+
+if args.port:
+    call(["netstat", "-pante"]) 
+
+if args.tab:
+    call(["netstat", "-r"])
